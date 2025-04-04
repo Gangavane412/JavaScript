b8bf7a08902784dfb0104dd8eedaeb9f4d47d524
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-04-04 20:00:17 +0530

    adding day 3 folder

---
+++ day3/script.js
@@ -0,0 +1,52 @@
+  
+// primitive DataType//
+
+// number daya type
+let otp=12334;
+let pi=3.14;
+let radius = 7;
+console.log("radius", radius);
+console.log(typeof(pi));
+
+
+
+// string 
+let name = "priyanka";
+var fanme ='priyanka';
+console.log( fname);
+console.log(typeof(name));
+
+//boolean
+// only two value true and flase
+let logined=true;
+let user_have_access=false;
+
+// null
+let a =null;
+console.log( typeof(a));
+
+
+
+//undefined
+let b;
+console.log( typeof(b));
+
+
+
+// Non-primitive DataType //
+
+// object
+let obj={
+    name:"priyanka",
+    age:21,
+    city:"shevagaon"
+};
+
+
+// array
+let arr=[1,2,3,4];
+
+// Function 
+ function sum (){
+    let num=1;
+};
