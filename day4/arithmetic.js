eef7e656e4e5d94f28256d51899e97a059ab57c2
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-04-05 20:03:15 +0530

    adding day 4 folder

---
+++ day4/arithmetic.js
@@ -0,0 +1,19 @@
+let a=12;
+let b=10;
+
+
+// addtion
+console.log(a+b);
+
+// subtraction
+console.log(a-b);
+
+// Multipilication
+console.log(a*b);
+
+// division
+console.log(a/b);
+
+//modilo
+console.log(a%b);
+
---
+++ day4/script.js
@@ -0,0 +1,18 @@
+let name ="welcome to javascript"
+
+console.log(name);
+
+let a=12;
+let b=10;
+console.log(a+b);
+
+
+let info ="learn js ";
+console.info(info);
+
+let warn="faild to feth";
+console.warn(warn);
+
+
+let err_message = "something went wrong";
+console.error(err_message);
