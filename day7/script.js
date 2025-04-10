9d2f9cbafa33df4f070529aaf19c73f676e0df1b
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-04-10 20:10:49 +0530

    adding day 7 folder

---
+++ day7/script.js
@@ -0,0 +1,16 @@
+console.log(document);// full document
+
+console.log(document.body); // for only body of html
+
+
+let newtitle = "welcomd to easy code";
+document.title= newtitle;
+
+
+document.write("<h3> addeing h3 </h3>");
+document.writeln(hello);
+
+let subHeading = " <p> we have  complete js </p>";
+document.writeln(subHeading);
+
+document.URL();
