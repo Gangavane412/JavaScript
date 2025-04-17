d201b05da45366a5692e8c7fa11dee96db6125b2
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-04-17 20:21:56 +0530

    adding day 8 flolde

---
+++ day8/index.js
@@ -0,0 +1,18 @@
+let ope1= prompt("enter 1  number:");
+let ope2 = prompt("enter 2 number:");
+
+
+
+
+// typecasting 
+let num1= parseInt(ope1);
+let num2= parseInt(poe2);
+
+
+console.log(ope1+ope2);
+console.log(ope1-ope2);
+console.log(ope1*ope2);
+console.log(ope1/ope2);
+
+let age = parseInt(prompt("enter your age:"));
+console.log(typeof(age));
---
+++ day8/script.js
@@ -0,0 +1,19 @@
+// alert("hiii");
+
+// 1.alert()
+let age =12;
+age <18?alert("are you able to vote"): alert("are you not able to vote");
+
+
+
+// 2. confirm()
+ let res = confirm("are you ready?");
+  console.log(res);
+
+
+// 3.  prompt()
+
+let result = prompt("enter your name");
+console.log(result);
+console.log(typeof(result));
+document.writeln("hello" + result +"welcom ");
