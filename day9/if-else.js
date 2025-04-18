281b7d216c2ee747ebc544b6498f49e2d970f9da
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-04-18 20:08:18 +0530

    adding day 9 flolde

---
+++ day9/if-else.js
@@ -0,0 +1,25 @@
+
+// if -else
+let age = parseInt(prompt("enter age"));
+if(age<18){
+    console.log("you are not able to vote!");
+    document.writeln(" you are not able to vote!")
+}
+else{
+    console.log("you are able to drive");
+    document.writeln("you are able to vote!");
+}
+
+
+
+
+let num = parseInt(prompt("enter num"));
+
+if(num%2==0){
+    console.log("number" +num +" is even");
+    document.writeln("number " +num +"  is even");
+}
+else{
+    console.log("number " +num +"  is odd");
+    document.writeln("number " +num +"  is odd");
+}
---
+++ day9/if.js
@@ -0,0 +1,21 @@
+// condition statement 
+
+// 1. if
+
+if(false){
+    console.log("heoll");
+}
+
+let age = parseInt(prompt("enter age"));
+if(age<18){
+    console.log("you are not able to vote!");
+    document.writeln(" you are not able to vote!")
+}
+if(age>18){
+    console.log("you are able to drive");
+    document.writeln("you are able to vote!");
+}
+
+
+// 2. if -else 
+
