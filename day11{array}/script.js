44f84d1deb3850b857ffe420aa9d7d92e75f7a63
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-05-05 20:04:06 +0530

    adding day11

---
+++ day11{array}/script.js
@@ -0,0 +1,19 @@
+let colors = []; //empty array   length =0
+
+let name = ["A" , "B", "C", "D"];
+console.log(name);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]); // give undefined
+
+
+console.log(name.legth());
+
+name.push("E");// adding element at last 
+name.unshift("0"); // adding element at first
+
+name.pop() // remoeve last element 
+
+name.shift() // remoe form fist element
