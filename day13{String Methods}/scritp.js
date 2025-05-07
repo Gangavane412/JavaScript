7c518fd7acd425693c878c6403d2af131062ed18
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-05-07 20:00:28 +0530

    adding day 13 folder

---
+++ day13{String Methods}/scritp.js
@@ -0,0 +1,28 @@
+let str= "helloe world !";
+
+
+//length
+console.log(str.length); //  14   -> space also count
+
+
+// toUpperCase
+console.log(str.toUpperCase());
+
+
+// toowerCase
+console.log(str.toLowerCase());
+
+
+//split ()
+let welComeMsg ="hello welcome in easyCOde lectures";//conver string into array
+console.log(welComeMsg.split(" "));  //  one word  
+console.log(welComeMsg.split("")); // one leter 
+
+
+let msg="helloThowTareTyou"
+console.log(msg.split("T"));// hello how are you
+
+
+
+let name= "Priyank Gangavane";
+console.log(name.split(" ")[0]); // priyanka
---
+++ day13{object proparties)/script.js
@@ -0,0 +1,31 @@
+let PhoneInfo ={
+    name:"iphone",
+    price:6500,
+    color:"black"
+}
+
+PhoneInfo.isHavaCharger =false;
+
+
+console.log(PhoneInfo);
+let key=Object.keys(PhoneInfo);
+console.log(key);
+
+let ObjValues = Object.values(PhoneInfo);
+console.log(ObjValues);
+
+
+
+let Dout={
+    doubt:"how i insert new kry in object"
+}
+
+console.log(Dout);
+
+Dout.isdoubtSolve= true
+Dout.answer="I write directly object name .newkey= value"
+Dout.data="12/1/2025"
+
+console.log(Dout);
+
+
