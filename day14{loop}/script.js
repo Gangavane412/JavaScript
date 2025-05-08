b8561cdae20a5a6957ebe856e745adea30404c13
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-05-08 23:42:03 +0530

    adding day 14 folder

---
+++ day14{loop}/script.js
@@ -0,0 +1,22 @@
+
+let num = Number(prompt("enter the number"))
+
+let count = 1 ;
+
+while(count <= 10){
+    document.writeln("<h1>" + num*count +"</h1>")
+    count++
+}
+
+
+
+
+
+let num1 = 50 ;
+
+do{
+    console.log("hello")
+    num1++
+    console.log(num1)
+
+}while(num1 < 5)
